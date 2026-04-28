331d5f1b9d2377db9217e3c83dce8d0c9083148a
reiols01 <94649576+reiols01@users.noreply.github.com>
2026-04-27 21:59:03 -0500

    Add files via upload

--- LCD_functions.h
+++ LCD_functions.h
@@ -1,48 +1,38 @@
-
 #ifndef LCD_FUNCTIONS_H
-#define	LCD_FUNCTIONS_H
+#define LCD_FUNCTIONS_H
 
-
-#include <string.h>
-#include <stdio.h> // Needed for sprintf
-#include "xc.h"    // Needed for SFR definitions
-
-#define FCY 16000000UL
-#include <libpic30.h>
-#include "LCD_base_library.h"
-#include "LCD_functions.h"
-
-#define DEFAULT 1
-#define SETTEMP 2
-#define SETTIME 3
-#define CONFIRM 4
-#define ALARM 5
-#define RESET 0
-#define UNPRESSED 6
-#define PRESSED 7
-
-
-#ifdef	__cplusplus
+#ifdef __cplusplus
 extern "C" {
-#endif /* __cplusplus */
+#endif
 
-  // Insert declarations
- void init_tmr1 (void);
+// ================= STATE DEFINITIONS =================
+#define DEFAULT   1
+#define SETTEMP   2
+#define SETTIME   3
+#define CONFIRM   4
+#define ALARM     5
+#define RESET     0
 
+#define UNPRESSED 6
+#define PRESSED   7
+
+// ================= FUNCTIONS =================
+void init_tmr1(void);
+
+void LCD_updateInputs(int x, int y, int button,int pir, double temp);
 void LCD_function_main(void);
 
-void default_state(void) ;
+void default_state(void);
+void settemp_state(void);
+void settimer_state(void);
+void confirmation_state(void);
+void alarm_state(void);
+void reset_state(void);
 
-void settemp_state(void) ;
+void print(char* s0, char* s1, char* s2, char* s3);
 
-void settimer_state (void);
-
-void confirmation_state (void);
-
-void alarm_state (void);
-
-void reset_state (void);
-
-void print(char* s0, char* s1, char* s2, char* s3); 
+#ifdef __cplusplus
+}
+#endif
 
 #endif
